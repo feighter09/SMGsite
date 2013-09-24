312a07b53491ddbdfe5f5d130efb8ef7610ee4de
Austin Feight <afeight@umich.edu>
2013-09-24 10:55:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SMGsite::Application.config.secret_key_base = '2abd701f1eef00eb3bb2e5b30c88714ec5450459a2d36884d15a30dfd161f4a8516a9c3ec0f5f82be225b293693f46584ff1b742fbb2508c13a6d20d4aa8bca7'
